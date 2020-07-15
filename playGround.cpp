ca566afa33412c01f3d0d083bf5eb1db3307d721
joo8070 <68212702+joo8070@users.noreply.github.com>
2020-07-15 15:06:16 +0900

    playGround
    delete init

--- playGround.cpp
+++ playGround.cpp
@@ -16,18 +16,6 @@ HRESULT playGround::init()
 {
 	gameNode::init(true);
 
-	INIDATA->addData("관우", "무력", "99");
-	INIDATA->addData("관우", "지력", "78");
-	INIDATA->addData("관우", "인내력", "200");
-
-	INIDATA->addData("건우", "무력", "95");
-	INIDATA->addData("건우", "도발력", "999");
-	INIDATA->addData("건우", "포텐", "100");
-
-	INIDATA->iniSave("도발인가");
-
-	_strength = INIDATA->loadDataInteger("도발인가","건우", "무력");
-	_intel = INIDATA->loadDataString("도발인가", "관우", "인내력");
 
 	return S_OK;
 }
